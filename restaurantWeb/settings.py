89c54d3fc32afe0e007a63fdd3a036eeefdefe7d
yuhan li <yuhanli@yuhans-iMac-Pro.local>
2020-04-01 01:34:53 +1300

    remove dynamic static

--- restaurantWeb/settings.py
+++ restaurantWeb/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
-MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 SITE_ID = 1
