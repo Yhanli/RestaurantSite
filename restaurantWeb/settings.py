639d6ba641fd2991104441c9c5c42cdc56c7f6bc
yuhan li <yuhanli@yuhans-iMac-Pro.local>
2020-04-01 01:49:05 +1300

    testing

--- restaurantWeb/settings.py
+++ restaurantWeb/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT ='static_root'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_ROOT = 'media_root'
 
 SITE_ID = 1
