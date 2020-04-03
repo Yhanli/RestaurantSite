e8a7b44601c1a7d881236d61f33b9967d18121b9
yuhan li <yuhanli@yuhans-iMac-Pro.local>
2020-04-03 20:29:55 +1300

    use alternative way to serve static and media

--- restaurantWeb/urls.py
+++ restaurantWeb/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import url
+from django.conf.urls.static import static, serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('website.urls', namespace='website')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
